ac49883d761141b4430308746031ea02c6da4af5
Abdo Najjar <raf7awe.overlord@gmail.com>
2020-12-21 23:29:36 +0200

    init commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-header">Example Component</div>
+
+      <div class="card-body">I'm an example component.</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    axios.get("/api/");
+  },
+};
+</script>
